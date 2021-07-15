e00195c706838690bb0d90f4dd6a354c4dc01509
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2021-07-14 21:11:45 -0300

    feat: install and config chakra-ui and overwrite default theme with extendTheme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
 
 export default function Home() {
-  return <h1>Dashgo</h1>
+  return (
+    <>
+      <h1>Dashgo</h1>
+      <p>Teste</p>
+      <p>Teste</p>
+      <p>Teste</p>
+    </>
+  )
 }
