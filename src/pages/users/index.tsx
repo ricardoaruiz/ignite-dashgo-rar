07c40d8f86cf706b612695ed21cbeaedf579a824
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2021-07-18 15:53:01 -0300

    feat: create initial version of list users page

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,144 @@
+import React from 'react'
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Text,
+  Icon,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from '@chakra-ui/react'
+import { Header } from '../../components'
+import { Sidebar } from '../../components/Sidebar/Sidebar'
+import { RiAddLine, RiPencilLine } from 'react-icons/ri'
+
+const UsersPage = () => {
+  return (
+    <Box>
+      <Header />
+
+      <Flex my="6" mx="auto" px="6" w="100%" maxWidth={1480}>
+        <Sidebar />
+
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justifyContent="space-between" alignItems="center">
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+            </Heading>
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} />}
+              cursor="pointer"
+            >
+              Criar novo usuário
+            </Button>
+          </Flex>
+
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px="6" color="gray.300" width="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>Usuário</Th>
+                <Th>Data de cadastro</Th>
+                <Th width="8"></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Ricardo Ruiz</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      ricardo.almendro.ruiz@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>04 de Abril, 2021</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} />}
+                    cursor="pointer"
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Ricardo Ruiz</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      ricardo.almendro.ruiz@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>04 de Abril, 2021</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} />}
+                    cursor="pointer"
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Ricardo Ruiz</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      ricardo.almendro.ruiz@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>04 de Abril, 2021</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} />}
+                    cursor="pointer"
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </Box>
+      </Flex>
+    </Box>
+  )
+}
+
+export default UsersPage
